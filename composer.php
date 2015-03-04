8e612ab0cdde7d43e420cec962bcdc28948c6949
Tom Rochette <roctom@gmail.com>
2015-03-04 16:12:50 -0500

    Create symbolic link to {release_path}/composer.phar (not composer.phar).

--- composer.php
+++ composer.php
@@ -4,7 +4,7 @@ task('composer:setup', function() {
 	cd('{deploy_path}/shared');
 	run('wget -nc http://getcomposer.org/composer.phar');
 	run('php composer.phar self-update');
-	run('ln -s {deploy_path}/shared/composer.phar composer.phar');
+	run('ln -s {deploy_path}/shared/composer.phar {release_path}/composer.phar');
 })->desc('Composer setup');
 
 task('composer:install', function() {
